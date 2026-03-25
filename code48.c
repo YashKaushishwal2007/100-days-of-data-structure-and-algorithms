1958df5f1cab8a6947282d146fb477a43a10eb80
Yashkaushiswal2007 <yash010707@gmail.com>
2026-03-25 21:48:43 +0530

    initial commit

---
+++ code48.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node {
+    int data;
+    struct Node *left, *right;
+} Node;
+
+Node* newNode(int data) {
+    Node* node = (Node*)malloc(sizeof(Node));
+    node->data = data;
+    node->left = node->right = NULL;
+    return node;
+}
+
+Node* buildTree(int arr[], int n) {
+    if (n == 0 || arr[0] == -1) return NULL;
+
+    Node** queue = (Node**)malloc(n * sizeof(Node*));
+    int front = 0, rear = 0;
+
+    Node* root = newNode(arr[0]);
+    queue[rear++] = root;
+
+    int i = 1;
+    while (i < n) {
+        Node* current = queue[front++];
+
+        if (arr[i] != -1) {
+            current->left = newNode(arr[i]);
+            queue[rear++] = current->left;
+        }
+        i++;
+
+        if (i < n && arr[i] != -1) {
+            current->right = newNode(arr[i]);
+            queue[rear++] = current->right;
+        }
+        i++;
+    }
+
+    free(queue);
+    return root;
+}
+
+int countLeaf(Node* root) {
+    if (!root) return 0;
+    if (!root->left && !root->right) return 1;
+    return countLeaf(root->left) + countLeaf(root->right);
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int* arr = (int*)malloc(n * sizeof(int));
+    for (int i = 0; i < n; i++) scanf("%d", &arr[i]);
+
+    Node* root = buildTree(arr, n);
+    printf("%d", countLeaf(root));
+
+    free(arr);
+    return 0;
+}
