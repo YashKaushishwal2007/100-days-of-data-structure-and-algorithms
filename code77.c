c494fca6b52836cf03ccc660fc9a3ac4f868e6e6
Yashkaushiswal2007 <yash010707@gmail.com>
2026-05-04 17:10:53 +0530

    initial commit

---
+++ code77.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+#define MAX 100
+
+int adj[MAX][MAX];
+int visited[MAX];
+
+void dfs(int v, int n) {
+    visited[v] = 1;
+    for (int i = 1; i <= n; i++) {
+        if (adj[v][i] && !visited[i]) {
+            dfs(i, n);
+        }
+    }
+}
+
+int main() {
+    int n, m;
+    scanf("%d %d", &n, &m);
+
+    for (int i = 1; i <= n; i++) {
+        visited[i] = 0;
+        for (int j = 1; j <= n; j++) {
+            adj[i][j] = 0;
+        }
+    }
+
+    for (int i = 0; i < m; i++) {
+        int u, v;
+        scanf("%d %d", &u, &v);
+        adj[u][v] = 1;
+        adj[v][u] = 1;
+    }
+
+    dfs(1, n);
+
+    for (int i = 1; i <= n; i++) {
+        if (!visited[i]) {
+            printf("NOT CONNECTED");
+            return 0;
+        }
+    }
+
+    printf("CONNECTED");
+    return 0;
+}
